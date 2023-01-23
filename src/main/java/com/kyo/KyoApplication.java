68732ff180934ed758c39b0a7352abe3247ba570
norbs <norbertbobila12@gmail.com>
2023-01-23 17:44:58 +0800

    try

--- src/main/java/com/kyo/KyoApplication.java
+++ src/main/java/com/kyo/KyoApplication.java
@@ -6,6 +6,7 @@ import com.kyo.Model.KyoModel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.math.BigDecimal;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -17,6 +18,12 @@ public class KyoApplication {
 
 
     public static void main(String[] args) {
+
+        BigDecimal rates = new BigDecimal("1.00");
+
+
+        BigDecimal rate = rates.multiply(new BigDecimal(0.01)).setScale(4,BigDecimal.ROUND_DOWN);
+        System.out.println(rate);
 
         List<KyoModel> kyoModelList = Arrays.asList(new KyoModel("Sheesh","sss"),new KyoModel("Sheesh1","sWERss"));
 
